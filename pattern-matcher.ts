a743423cbe1473d75fff8d24a1b67f5e7bf6a29c
Ukhang <ukhangmarma459@gmail.com>
2025-03-04 16:59:23 +0600

    add matchAlphanumeric method to PatternMatcher

--- pattern-matcher.ts
+++ pattern-matcher.ts
@@ -1,5 +1,9 @@
 export class PatternMatcher {
     static matchDigits(text: string): boolean {
         return /\d/.test(text);
-      }    
+    }
+
+    static matchAlphanumeric(text: string): boolean {
+        return /^[a-zA-Z0-9]+$/.test(text);
+    }
 }
