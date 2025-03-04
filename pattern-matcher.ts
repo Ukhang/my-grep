183971df1d16dad1a5bf02a1a42b61e647a93d8e
Ukhang <ukhangmarma459@gmail.com>
2025-03-04 17:04:52 +0600

    add alternation method to PatternMatcher

--- pattern-matcher.ts
+++ pattern-matcher.ts
@@ -39,4 +39,9 @@ export class PatternMatcher {
         const regex = new RegExp(`${pattern}`);
         return regex.test(text);
     }
+
+    static alternation(text: string, pattern: string): boolean {
+        const regex = new RegExp(pattern);
+        return regex.test(text);
+    }
 }
